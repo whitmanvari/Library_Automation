b3fca05d7d63d7711cd869fb2798b3559e4e22cc
İraz Hazal İlik <ilikhazal16@gmail.com>
2025-02-13 00:30:36 +0300

    Add Book class to Library.Model.Concrete namespace
    Introduce a new class `Book` with properties to store information about a book, including `Id`, `BookName`, `BookAuthor`, `BookPublisher`, `BookPage`, `BookYear`, `BookLanguage`, `BookGenre`, and `BookDescription`.

--- Library/Model/Concrete/Book.cs
+++ Library/Model/Concrete/Book.cs
@@ -9,5 +9,16 @@ namespace Library.Model.Concrete
     public class Book
     {
         //Book information.
+        public int Id { get; set; }
+        public string BookName { get; set; }
+        public string BookAuthor { get; set; }
+        public string BookPublisher { get; set; }
+        public int BookPage { get; set; }
+        //Book publish year.
+        public int BookYear { get; set; }
+        public string BookLanguage { get; set; }
+        public string BookGenre { get; set; }
+        public string BookDescription { get; set; }
+
     }
 }
