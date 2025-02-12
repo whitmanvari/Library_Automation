252d5d97a4dd7d5ae0eabcaf0e4f39f00033bdce
İraz Hazal İlik <ilikhazal16@gmail.com>
2025-02-13 01:28:09 +0300

    Add Entity Framework namespace and initialize connection string
    Included System.Data.Entity namespace for Entity Framework support.
    Set Database.Connection.ConnectionString to an empty string in the
    DataContext constructor to allow for future connection string setup.

--- Library/DataContext/DataContext.cs
+++ Library/DataContext/DataContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,7 @@ namespace Library.DataContext
         //Mssql connection string will be add.
         public DataContext()
         {
-            
+            Database.Connection.ConnectionString = "";
         }
     }
 }
