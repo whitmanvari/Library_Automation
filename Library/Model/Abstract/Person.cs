0e78c5be0d6c9e07eab5f705b5ed6b2fd0171366
İraz Hazal İlik <ilikhazal16@gmail.com>
2025-02-12 17:44:22 +0300

    Add abstract Person class and update project file
    Introduce abstract Person class in Person.cs with properties Id, Name, Surname, Email, BirthDate, Password, Phone, and Address. Update Library.csproj to include Person.cs in the build process.

---
+++ Library/Model/Abstract/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Model.Abstract
+{
+    public abstract class Person
+    {
+        public abstract int Id { get; set; }
+        public abstract string Name { get; set; }
+        public abstract string Surname { get; set; }
+        public abstract string Email { get; set; }
+        public abstract DateTime BirthDate { get; set; }
+        public abstract string Password { get; set; }
+        public abstract string Phone { get; set; }
+        public abstract string Address { get; set; }
+    }
+}
