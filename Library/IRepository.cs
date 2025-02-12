f18ff64028ab240889af58e192c358b39d3344c2
İraz Hazal İlik <ilikhazal16@gmail.com>
2025-02-12 16:10:56 +0300

    I have created a new interface for the basic CRUD operations. It has a generic type in it. The `Library.csproj` file was updated to include the new `IRepository.cs` file in the project.# The `IRepository.cs` file was created, containing an interface `IRepository<T>` with methods for basic CRUD operations.

---
+++ Library/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public interface IRepository<T>
+    {
+        //Basic CRUD operations.
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        void Get(T entity);
+    }
+}
