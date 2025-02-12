6587db38f7bc9d1a412863d64a2888f03199ac11
İraz Hazal İlik <ilikhazal16@gmail.com>
2025-02-13 00:52:44 +0300

    Add Presentation class and restructure project folders
    Updated Library.csproj to include Presentation.cs in View\Presentation.
    Removed Controller\ and View\ folders, added Controller\Services\Business Layer\.
    Added Presentation class in Presentation.cs with initial setup.

---
+++ Library/View/Presentation/Presentation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Library.View.Presentation
+{
+    internal class Presentation
+    {
+        //Library.Presentation include UI Layer components
+       
+    }
+}
