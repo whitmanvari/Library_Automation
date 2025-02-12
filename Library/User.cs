3159d22584e0ca56fdbdd2bff8318fe4f25733e0
İraz Hazal İlik <ilikhazal16@gmail.com>
2025-02-12 16:40:11 +0300

    Add User class and update project file
    Added a new `User` class in `User.cs` with private fields for `id`, `name`, `surname`, `birthDate`, `email`, and `password` within the `Library` namespace. Updated `Library.csproj` to include `User.cs` in the project compilation.

---
+++ Library/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class User
+    {
+        private int _id;
+        private string _name;
+        private string _surname;
+        private DateTime _birthDate;
+        private string _email;
+        private string _password;
+    }
+}
