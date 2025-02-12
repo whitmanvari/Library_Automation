cde90342dfd7e5dd4191663d69b90f57e68ef828
İraz Hazal İlik <ilikhazal16@gmail.com>
2025-02-13 01:08:18 +0300

    Add BookRepository and MemberRepository classes
    Updated Library.csproj to include new source files: BookRepository.cs and MemberRepository.cs. Added BookRepository and MemberRepository classes in the Model\Concrete namespace, both implementing IRepository for their respective entities. All methods currently throw NotImplementedException.

---
+++ Library/Model/Concrete/BookRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Model.Concrete
+{
+    public class BookRepository : IRepository<Book>
+    {
+        public void Add(Book entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Book entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Book> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Book entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Library/Model/Concrete/MemberRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Model.Concrete
+{
+    public class MemberRepository : IRepository<Member>
+    {
+        public void Add(Member entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Member entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Member> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Member entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
