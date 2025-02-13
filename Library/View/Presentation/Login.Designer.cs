e211ca7058d146ff603b8e7b7d869c3589b0a8c2
İraz Hazal İlik <ilikhazal16@gmail.com>
2025-02-13 13:42:00 +0300

    Refactor Admin Login UI and update resource management
    - Updated InitializeComponent to include ComponentResourceManager.
    - Renamed tabPage1 to tabPage_AdminLogin and updated its properties.
    - Added pictureBox1, label_TitleLogin, and panel1 to the form.
    - Modified tabControl1 properties to include a new font and location.
    - Updated tabPage_AdminLogin with new properties, including background color, border style, and controls.
    - Updated lbl_AdminRole_Login, lbl_AdminEmail_Login, and lbl_AdminPassword_Login labels with new fonts, locations, and sizes.
    - Repositioned txt_AdminEmail_Login and txt_AdminPassword_Login text boxes.
    - Updated button_Enter_LoginPage with a new font, location, and size.
    - Updated linkLabel_AdminLogin with new colors, location, and size.
    - Repositioned comboBox1.
    - Updated btn_ExitLoginPage with a new font, location, and size.
    - Added pictureBox1 with a background image and specific properties.
    - Added label_TitleLogin with specific font, color, and location properties.
    - Added panel1 to the top of the form with a specific background color and size.
    - Updated form's ClientSize to a new size.
    - Replaced tabPage1 references with tabPage_AdminLogin references.
    - Added new controls (label_TitleLogin, pictureBox1, panel1) to the form's control collection.

--- Library/View/Presentation/Login.Designer.cs
+++ Library/View/Presentation/Login.Designer.cs
@@ -29,8 +29,9 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage_AdminLogin = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.lbl_AdminRole_Login = new System.Windows.Forms.Label();
             this.lbl_AdminEmail_Login = new System.Windows.Forms.Label();
@@ -44,39 +45,49 @@
             this.adminsBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.adminsTableAdapter = new Library.LibraryAutomationDataSet3TableAdapters.AdminsTableAdapter();
             this.btn_ExitLoginPage = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label_TitleLogin = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.tabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
+            this.tabPage_AdminLogin.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.libraryAutomationDataSet3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.adminsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage_AdminLogin);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Font = new System.Drawing.Font("Segoe Print", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl1.Location = new System.Drawing.Point(12, 47);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(496, 461);
+            this.tabControl1.Size = new System.Drawing.Size(606, 407);
             this.tabControl1.TabIndex = 0;
             // 
-            // tabPage1
+            // tabPage_AdminLogin
             // 
-            this.tabPage1.Controls.Add(this.comboBox1);
-            this.tabPage1.Controls.Add(this.linkLabel_AdminLogin);
-            this.tabPage1.Controls.Add(this.button_Enter_LoginPage);
-            this.tabPage1.Controls.Add(this.txt_AdminPassword_Login);
-            this.tabPage1.Controls.Add(this.txt_AdminEmail_Login);
-            this.tabPage1.Controls.Add(this.lbl_AdminPassword_Login);
-            this.tabPage1.Controls.Add(this.lbl_AdminEmail_Login);
-            this.tabPage1.Controls.Add(this.lbl_AdminRole_Login);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(488, 435);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "Admin Login";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage_AdminLogin.BackColor = System.Drawing.Color.Transparent;
+            this.tabPage_AdminLogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.tabPage_AdminLogin.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tabPage_AdminLogin.Controls.Add(this.pictureBox1);
+            this.tabPage_AdminLogin.Controls.Add(this.comboBox1);
+            this.tabPage_AdminLogin.Controls.Add(this.linkLabel_AdminLogin);
+            this.tabPage_AdminLogin.Controls.Add(this.button_Enter_LoginPage);
+            this.tabPage_AdminLogin.Controls.Add(this.txt_AdminPassword_Login);
+            this.tabPage_AdminLogin.Controls.Add(this.txt_AdminEmail_Login);
+            this.tabPage_AdminLogin.Controls.Add(this.lbl_AdminPassword_Login);
+            this.tabPage_AdminLogin.Controls.Add(this.lbl_AdminEmail_Login);
+            this.tabPage_AdminLogin.Controls.Add(this.lbl_AdminRole_Login);
+            this.tabPage_AdminLogin.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.tabPage_AdminLogin.Location = new System.Drawing.Point(4, 30);
+            this.tabPage_AdminLogin.Name = "tabPage_AdminLogin";
+            this.tabPage_AdminLogin.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_AdminLogin.Size = new System.Drawing.Size(598, 373);
+            this.tabPage_AdminLogin.TabIndex = 0;
+            this.tabPage_AdminLogin.Text = "Admin Login";
             // 
             // tabPage2
             // 
@@ -91,37 +102,37 @@
             // lbl_AdminRole_Login
             // 
             this.lbl_AdminRole_Login.AutoSize = true;
-            this.lbl_AdminRole_Login.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_AdminRole_Login.Location = new System.Drawing.Point(60, 134);
+            this.lbl_AdminRole_Login.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_AdminRole_Login.Location = new System.Drawing.Point(6, 111);
             this.lbl_AdminRole_Login.Name = "lbl_AdminRole_Login";
-            this.lbl_AdminRole_Login.Size = new System.Drawing.Size(52, 25);
+            this.lbl_AdminRole_Login.Size = new System.Drawing.Size(61, 33);
             this.lbl_AdminRole_Login.TabIndex = 0;
             this.lbl_AdminRole_Login.Text = "Role:";
             // 
             // lbl_AdminEmail_Login
             // 
             this.lbl_AdminEmail_Login.AutoSize = true;
-            this.lbl_AdminEmail_Login.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_AdminEmail_Login.Location = new System.Drawing.Point(60, 203);
+            this.lbl_AdminEmail_Login.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_AdminEmail_Login.Location = new System.Drawing.Point(6, 176);
             this.lbl_AdminEmail_Login.Name = "lbl_AdminEmail_Login";
-            this.lbl_AdminEmail_Login.Size = new System.Drawing.Size(62, 25);
+            this.lbl_AdminEmail_Login.Size = new System.Drawing.Size(74, 33);
             this.lbl_AdminEmail_Login.TabIndex = 0;
             this.lbl_AdminEmail_Login.Text = "Email:";
             // 
             // lbl_AdminPassword_Login
             // 
             this.lbl_AdminPassword_Login.AutoSize = true;
-            this.lbl_AdminPassword_Login.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_AdminPassword_Login.Location = new System.Drawing.Point(60, 275);
+            this.lbl_AdminPassword_Login.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_AdminPassword_Login.Location = new System.Drawing.Point(6, 238);
             this.lbl_AdminPassword_Login.Name = "lbl_AdminPassword_Login";
-            this.lbl_AdminPassword_Login.Size = new System.Drawing.Size(95, 25);
+            this.lbl_AdminPassword_Login.Size = new System.Drawing.Size(112, 33);
             this.lbl_AdminPassword_Login.TabIndex = 0;
             this.lbl_AdminPassword_Login.Text = "Password:";
             // 
             // txt_AdminEmail_Login
             // 
             this.txt_AdminEmail_Login.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_AdminEmail_Login.Location = new System.Drawing.Point(167, 203);
+            this.txt_AdminEmail_Login.Location = new System.Drawing.Point(124, 176);
             this.txt_AdminEmail_Login.Name = "txt_AdminEmail_Login";
             this.txt_AdminEmail_Login.Size = new System.Drawing.Size(225, 23);
             this.txt_AdminEmail_Login.TabIndex = 1;
@@ -129,29 +140,29 @@
             // txt_AdminPassword_Login
             // 
             this.txt_AdminPassword_Login.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_AdminPassword_Login.Location = new System.Drawing.Point(167, 275);
+            this.txt_AdminPassword_Login.Location = new System.Drawing.Point(124, 248);
             this.txt_AdminPassword_Login.Name = "txt_AdminPassword_Login";
             this.txt_AdminPassword_Login.Size = new System.Drawing.Size(225, 23);
             this.txt_AdminPassword_Login.TabIndex = 1;
             // 
             // button_Enter_LoginPage
             // 
-            this.button_Enter_LoginPage.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_Enter_LoginPage.Location = new System.Drawing.Point(253, 336);
+            this.button_Enter_LoginPage.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Enter_LoginPage.Location = new System.Drawing.Point(210, 290);
             this.button_Enter_LoginPage.Name = "button_Enter_LoginPage";
-            this.button_Enter_LoginPage.Size = new System.Drawing.Size(139, 31);
+            this.button_Enter_LoginPage.Size = new System.Drawing.Size(139, 36);
             this.button_Enter_LoginPage.TabIndex = 2;
             this.button_Enter_LoginPage.Text = "Enter";
             this.button_Enter_LoginPage.UseVisualStyleBackColor = true;
             // 
             // linkLabel_AdminLogin
             // 
-            this.linkLabel_AdminLogin.ActiveLinkColor = System.Drawing.Color.Maroon;
+            this.linkLabel_AdminLogin.ActiveLinkColor = System.Drawing.Color.DarkMagenta;
             this.linkLabel_AdminLogin.AutoSize = true;
-            this.linkLabel_AdminLogin.LinkColor = System.Drawing.Color.Maroon;
-            this.linkLabel_AdminLogin.Location = new System.Drawing.Point(250, 370);
+            this.linkLabel_AdminLogin.LinkColor = System.Drawing.Color.Plum;
+            this.linkLabel_AdminLogin.Location = new System.Drawing.Point(169, 329);
             this.linkLabel_AdminLogin.Name = "linkLabel_AdminLogin";
-            this.linkLabel_AdminLogin.Size = new System.Drawing.Size(142, 13);
+            this.linkLabel_AdminLogin.Size = new System.Drawing.Size(180, 21);
             this.linkLabel_AdminLogin.TabIndex = 3;
             this.linkLabel_AdminLogin.TabStop = true;
             this.linkLabel_AdminLogin.Text = "Don\'t you have an account?";
@@ -163,7 +174,7 @@
             this.comboBox1.DataSource = this.adminsBindingSource;
             this.comboBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(167, 138);
+            this.comboBox1.Location = new System.Drawing.Point(124, 121);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(225, 23);
             this.comboBox1.TabIndex = 4;
@@ -184,28 +195,64 @@
             // 
             // btn_ExitLoginPage
             // 
-            this.btn_ExitLoginPage.Location = new System.Drawing.Point(410, 479);
+            this.btn_ExitLoginPage.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ExitLoginPage.Location = new System.Drawing.Point(517, 456);
             this.btn_ExitLoginPage.Name = "btn_ExitLoginPage";
-            this.btn_ExitLoginPage.Size = new System.Drawing.Size(75, 23);
+            this.btn_ExitLoginPage.Size = new System.Drawing.Size(97, 44);
             this.btn_ExitLoginPage.TabIndex = 1;
             this.btn_ExitLoginPage.Text = "Exit";
             this.btn_ExitLoginPage.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(355, -30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(279, 386);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label_TitleLogin
+            // 
+            this.label_TitleLogin.AutoSize = true;
+            this.label_TitleLogin.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_TitleLogin.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label_TitleLogin.Location = new System.Drawing.Point(12, 9);
+            this.label_TitleLogin.Name = "label_TitleLogin";
+            this.label_TitleLogin.Size = new System.Drawing.Size(186, 25);
+            this.label_TitleLogin.TabIndex = 2;
+            this.label_TitleLogin.Text = "Library Automation";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Plum;
+            this.panel1.Controls.Add(this.label_TitleLogin);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(646, 41);
+            this.panel1.TabIndex = 2;
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(509, 509);
+            this.ClientSize = new System.Drawing.Size(646, 509);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.btn_ExitLoginPage);
             this.Controls.Add(this.tabControl1);
             this.Name = "Login";
             this.Text = "Login";
             this.Load += new System.EventHandler(this.Login_Load);
             this.tabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
-            this.tabPage1.PerformLayout();
+            this.tabPage_AdminLogin.ResumeLayout(false);
+            this.tabPage_AdminLogin.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.libraryAutomationDataSet3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.adminsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -213,7 +260,7 @@
         #endregion
 
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage_AdminLogin;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TextBox txt_AdminPassword_Login;
         private System.Windows.Forms.TextBox txt_AdminEmail_Login;
@@ -227,5 +274,8 @@
         private System.Windows.Forms.BindingSource adminsBindingSource;
         private LibraryAutomationDataSet3TableAdapters.AdminsTableAdapter adminsTableAdapter;
         private System.Windows.Forms.Button btn_ExitLoginPage;
+        private System.Windows.Forms.Label label_TitleLogin;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
