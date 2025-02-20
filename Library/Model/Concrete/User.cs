9a9556b6009c39d658384c0fb5f0d4a7c782203c
İraz Hazal İlik <ilikhazal16@gmail.com>
2025-02-21 00:15:50 +0300

    Refactor User class by removing several properties
    The User class in the Library.Model.Concrete namespace has been
    refactored. The following properties have been removed: BirthDate,
    Name, Surname, Email, Password, Phone, Address, and Id. The
    remaining properties are CreatedDate, RoleId, and Role.

--- Library/Model/Concrete/User.cs
+++ Library/Model/Concrete/User.cs
@@ -10,18 +10,8 @@ namespace Library.Model.Concrete
 {
     public class User: Person
     {
-
-       
         public DateTime? BirthDate { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.Now;
-        public string Name { get; set; }
-        public string Surname { get; set; }
-
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string Phone { get; set; }
-        public string Address { get; set; }
-        public int Id { get; set; }
         public int RoleId { get; set; }
         public virtual Role Role { get; set; }
     }
