1a189b263c1bc613b3687c9da052db93f27525f4
İraz Hazal İlik <ilikhazal16@gmail.com>
2025-02-22 15:28:41 +0300

    Add category buttons and adjust UI elements in MainMenu
    Added new buttons for book categories (Horror/Thriller, Science Fiction, Romance, etc.) and a label ("Categories:") to the form in `Member_MainMenu.Designer.cs`. Adjusted the size and location of existing UI elements such as `toolStripSeparator1`, `toolStripButton_loanpage`, `toolStripButton1`, `label1`, `label_memberName`, and `pictureBox1`. Increased the overall form size from 800x450 to 800x684 to accommodate the new elements. Declared the new buttons and label as private members of the form class.

--- Library/View/Presentation/Member_MainMenu.Designer.cs
+++ Library/View/Presentation/Member_MainMenu.Designer.cs
@@ -46,6 +46,19 @@
             this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
             this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
             this.toolStripLabel_myProfile = new System.Windows.Forms.ToolStripLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.toolStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -127,7 +140,7 @@
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 37);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
             // 
             // toolStripButton_loanpage
             // 
@@ -136,7 +149,7 @@
             this.toolStripButton_loanpage.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton_loanpage.Image")));
             this.toolStripButton_loanpage.ImageTransparentColor = System.Drawing.Color.Thistle;
             this.toolStripButton_loanpage.Name = "toolStripButton_loanpage";
-            this.toolStripButton_loanpage.Size = new System.Drawing.Size(23, 34);
+            this.toolStripButton_loanpage.Size = new System.Drawing.Size(23, 22);
             this.toolStripButton_loanpage.Text = "Book Loan";
             this.toolStripButton_loanpage.ToolTipText = "Book Loan Page";
             this.toolStripButton_loanpage.MouseLeave += new System.EventHandler(this.toolStripButton_loanpage_MouseLeave);
@@ -153,10 +166,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 99);
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 84);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(89, 25);
+            this.label1.Size = new System.Drawing.Size(89, 27);
             this.label1.TabIndex = 5;
             this.label1.Text = "Welcome ";
             // 
@@ -164,7 +177,7 @@
             // 
             this.label_memberName.AutoSize = true;
             this.label_memberName.Font = new System.Drawing.Font("Segoe Script", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_memberName.Location = new System.Drawing.Point(107, 93);
+            this.label_memberName.Location = new System.Drawing.Point(109, 78);
             this.label_memberName.Name = "label_memberName";
             this.label_memberName.Size = new System.Drawing.Size(0, 33);
             this.label_memberName.TabIndex = 0;
@@ -173,16 +186,16 @@
             // 
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox1.Location = new System.Drawing.Point(469, 109);
+            this.pictureBox1.Location = new System.Drawing.Point(402, 162);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(416, 398);
+            this.pictureBox1.Size = new System.Drawing.Size(490, 541);
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
             // 
             // toolStripSeparator2
             // 
             this.toolStripSeparator2.Name = "toolStripSeparator2";
-            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 37);
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
             // 
             // toolStripButton1
             // 
@@ -191,7 +204,7 @@
             this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
             this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(23, 34);
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
             this.toolStripButton1.Text = "My Profile";
             this.toolStripButton1.TextDirection = System.Windows.Forms.ToolStripTextDirection.Vertical90;
             this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
@@ -206,11 +219,167 @@
             this.toolStripLabel_myProfile.Text = "My Profile";
             this.toolStripLabel_myProfile.Visible = false;
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe Script", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(36, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 36);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Horror/Thriller";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.button2.Font = new System.Drawing.Font("Segoe Script", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.button2.Location = new System.Drawing.Point(217, 192);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 36);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Science Fiction";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.button3.Font = new System.Drawing.Font("Segoe Script", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.button3.Location = new System.Drawing.Point(217, 281);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(152, 36);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Romance";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe Script", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(36, 245);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(152, 36);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Fantasy";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Segoe Script", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(36, 330);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(152, 36);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Historical Fiction";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.button6.Font = new System.Drawing.Font("Segoe Script", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.button6.Location = new System.Drawing.Point(217, 366);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(152, 36);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Literary Fiction";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.button7.Font = new System.Drawing.Font("Segoe Script", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.button7.Location = new System.Drawing.Point(217, 456);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(152, 36);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Poetry";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Segoe Script", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(36, 417);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(152, 36);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Fairy-Tale";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Segoe Script", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(36, 509);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(152, 36);
+            this.button9.TabIndex = 7;
+            this.button9.Text = "Dystopian";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.button10.Font = new System.Drawing.Font("Segoe Script", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.button10.Location = new System.Drawing.Point(217, 543);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(152, 36);
+            this.button10.TabIndex = 7;
+            this.button10.Text = "Adventure Fiction";
+            this.button10.UseVisualStyleBackColor = false;
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.button11.Font = new System.Drawing.Font("Segoe Script", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.button11.Location = new System.Drawing.Point(217, 624);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(152, 36);
+            this.button11.TabIndex = 7;
+            this.button11.Text = "Philosophy";
+            this.button11.UseVisualStyleBackColor = false;
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("Segoe Script", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button12.Location = new System.Drawing.Point(36, 594);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(152, 36);
+            this.button12.TabIndex = 7;
+            this.button12.Text = "Biography";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.label2.Location = new System.Drawing.Point(14, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 27);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Categories:";
+            // 
             // Member_MainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 684);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label_memberName);
             this.Controls.Add(this.label1);
@@ -247,5 +416,18 @@
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
         private System.Windows.Forms.ToolStripButton toolStripButton1;
         private System.Windows.Forms.ToolStripLabel toolStripLabel_myProfile;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Label label2;
     }
 }
