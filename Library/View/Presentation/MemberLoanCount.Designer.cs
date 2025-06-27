bf2292e5402ccd612b57ab1c2fac4baf4ffca3c8
hazal ilik <ilikhazal16@gmail.com>
2025-06-27 13:10:54 +0300

    Refactor MemberLoanCount form for loan period selection
    Refactored the MemberLoanCount form UI to replace name/surname textboxes with controls for selecting checkout date (DateTimePicker) and loan period (NumericUpDown). Added labels and a display for the expected return date. Updated button event handling to calculate and validate loan/return dates, showing warnings for invalid input. Increased form size and repositioned controls for improved layout. Removed obsolete member name/surname fields and related code.

--- Library/View/Presentation/MemberLoanCount.Designer.cs
+++ Library/View/Presentation/MemberLoanCount.Designer.cs
@@ -30,17 +30,20 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MemberLoanCount));
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_Loan = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.textBox_surnamemember = new System.Windows.Forms.TextBox();
-            this.textBox_namemember = new System.Windows.Forms.TextBox();
             this.label_memberSurname_RegisterPage = new System.Windows.Forms.Label();
             this.label_MemberName_RegisterPage = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label_MemberUpdate_SignIn = new System.Windows.Forms.Label();
+            this.label_MemberLoanCount_SignIn = new System.Windows.Forms.Label();
             this.label_TitleLogin = new System.Windows.Forms.Label();
+            this.nud_ReturnDate = new System.Windows.Forms.NumericUpDown();
+            this.dtp_checkoutDate = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_expectedReturnDate = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_ReturnDate)).BeginInit();
             this.SuspendLayout();
             // 
             // linkLabel1
@@ -49,24 +52,25 @@
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.linkLabel1.LinkColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.linkLabel1.Location = new System.Drawing.Point(470, 345);
+            this.linkLabel1.Location = new System.Drawing.Point(524, 402);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(108, 33);
             this.linkLabel1.TabIndex = 46;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Go Back ";
             // 
-            // button1
+            // btn_Loan
             // 
-            this.button1.BackColor = System.Drawing.Color.Black;
-            this.button1.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(100, 295);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(456, 38);
-            this.button1.TabIndex = 45;
-            this.button1.Text = "Loan";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btn_Loan.BackColor = System.Drawing.Color.Black;
+            this.btn_Loan.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Loan.ForeColor = System.Drawing.Color.White;
+            this.btn_Loan.Location = new System.Drawing.Point(99, 293);
+            this.btn_Loan.Name = "btn_Loan";
+            this.btn_Loan.Size = new System.Drawing.Size(456, 38);
+            this.btn_Loan.TabIndex = 45;
+            this.btn_Loan.Text = "Loan";
+            this.btn_Loan.UseVisualStyleBackColor = false;
+            this.btn_Loan.Click += new System.EventHandler(this.btn_Loan_Click);
             // 
             // pictureBox1
             // 
@@ -78,37 +82,21 @@
             this.pictureBox1.TabIndex = 44;
             this.pictureBox1.TabStop = false;
             // 
-            // textBox_surnamemember
-            // 
-            this.textBox_surnamemember.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox_surnamemember.Location = new System.Drawing.Point(286, 215);
-            this.textBox_surnamemember.Name = "textBox_surnamemember";
-            this.textBox_surnamemember.Size = new System.Drawing.Size(201, 39);
-            this.textBox_surnamemember.TabIndex = 31;
-            // 
-            // textBox_namemember
-            // 
-            this.textBox_namemember.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox_namemember.Location = new System.Drawing.Point(286, 170);
-            this.textBox_namemember.Name = "textBox_namemember";
-            this.textBox_namemember.Size = new System.Drawing.Size(201, 39);
-            this.textBox_namemember.TabIndex = 30;
-            // 
             // label_memberSurname_RegisterPage
             // 
             this.label_memberSurname_RegisterPage.AutoSize = true;
             this.label_memberSurname_RegisterPage.Font = new System.Drawing.Font("Segoe Print", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_memberSurname_RegisterPage.Location = new System.Drawing.Point(149, 224);
+            this.label_memberSurname_RegisterPage.Location = new System.Drawing.Point(149, 242);
             this.label_memberSurname_RegisterPage.Name = "label_memberSurname_RegisterPage";
-            this.label_memberSurname_RegisterPage.Size = new System.Drawing.Size(131, 30);
+            this.label_memberSurname_RegisterPage.Size = new System.Drawing.Size(130, 30);
             this.label_memberSurname_RegisterPage.TabIndex = 42;
-            this.label_memberSurname_RegisterPage.Text = "Return Date: ";
+            this.label_memberSurname_RegisterPage.Text = "Loan Period: ";
             // 
             // label_MemberName_RegisterPage
             // 
             this.label_MemberName_RegisterPage.AutoSize = true;
             this.label_MemberName_RegisterPage.Font = new System.Drawing.Font("Segoe Print", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_MemberName_RegisterPage.Location = new System.Drawing.Point(149, 179);
+            this.label_MemberName_RegisterPage.Location = new System.Drawing.Point(149, 200);
             this.label_MemberName_RegisterPage.Name = "label_MemberName_RegisterPage";
             this.label_MemberName_RegisterPage.Size = new System.Drawing.Size(142, 30);
             this.label_MemberName_RegisterPage.TabIndex = 43;
@@ -117,7 +105,7 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Black;
-            this.panel1.Controls.Add(this.label_MemberUpdate_SignIn);
+            this.panel1.Controls.Add(this.label_MemberLoanCount_SignIn);
             this.panel1.Controls.Add(this.label_TitleLogin);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
@@ -125,16 +113,16 @@
             this.panel1.Size = new System.Drawing.Size(644, 41);
             this.panel1.TabIndex = 29;
             // 
-            // label_MemberUpdate_SignIn
+            // label_MemberLoanCount_SignIn
             // 
-            this.label_MemberUpdate_SignIn.AutoSize = true;
-            this.label_MemberUpdate_SignIn.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_MemberUpdate_SignIn.ForeColor = System.Drawing.Color.White;
-            this.label_MemberUpdate_SignIn.Location = new System.Drawing.Point(401, 9);
-            this.label_MemberUpdate_SignIn.Name = "label_MemberUpdate_SignIn";
-            this.label_MemberUpdate_SignIn.Size = new System.Drawing.Size(90, 33);
-            this.label_MemberUpdate_SignIn.TabIndex = 7;
-            this.label_MemberUpdate_SignIn.Text = "Sign In";
+            this.label_MemberLoanCount_SignIn.AutoSize = true;
+            this.label_MemberLoanCount_SignIn.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_MemberLoanCount_SignIn.ForeColor = System.Drawing.Color.White;
+            this.label_MemberLoanCount_SignIn.Location = new System.Drawing.Point(542, 7);
+            this.label_MemberLoanCount_SignIn.Name = "label_MemberLoanCount_SignIn";
+            this.label_MemberLoanCount_SignIn.Size = new System.Drawing.Size(90, 33);
+            this.label_MemberLoanCount_SignIn.TabIndex = 7;
+            this.label_MemberLoanCount_SignIn.Text = "Sign In";
             // 
             // label_TitleLogin
             // 
@@ -147,24 +135,62 @@
             this.label_TitleLogin.TabIndex = 2;
             this.label_TitleLogin.Text = "Library Automation";
             // 
+            // nud_ReturnDate
+            // 
+            this.nud_ReturnDate.Location = new System.Drawing.Point(297, 251);
+            this.nud_ReturnDate.Name = "nud_ReturnDate";
+            this.nud_ReturnDate.Size = new System.Drawing.Size(200, 20);
+            this.nud_ReturnDate.TabIndex = 2;
+            // 
+            // dtp_checkoutDate
+            // 
+            this.dtp_checkoutDate.Location = new System.Drawing.Point(297, 205);
+            this.dtp_checkoutDate.Name = "dtp_checkoutDate";
+            this.dtp_checkoutDate.Size = new System.Drawing.Size(200, 20);
+            this.dtp_checkoutDate.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 10F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(63, 382);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 30);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Expected Return Date: ";
+            // 
+            // lbl_expectedReturnDate
+            // 
+            this.lbl_expectedReturnDate.AutoSize = true;
+            this.lbl_expectedReturnDate.Font = new System.Drawing.Font("Segoe Print", 10F, System.Drawing.FontStyle.Bold);
+            this.lbl_expectedReturnDate.ForeColor = System.Drawing.Color.Maroon;
+            this.lbl_expectedReturnDate.Location = new System.Drawing.Point(301, 382);
+            this.lbl_expectedReturnDate.Name = "lbl_expectedReturnDate";
+            this.lbl_expectedReturnDate.Size = new System.Drawing.Size(0, 30);
+            this.lbl_expectedReturnDate.TabIndex = 4;
+            // 
             // MemberLoanCount
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(644, 398);
+            this.ClientSize = new System.Drawing.Size(644, 444);
+            this.Controls.Add(this.lbl_expectedReturnDate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtp_checkoutDate);
+            this.Controls.Add(this.nud_ReturnDate);
             this.Controls.Add(this.linkLabel1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_Loan);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.textBox_surnamemember);
-            this.Controls.Add(this.textBox_namemember);
             this.Controls.Add(this.label_memberSurname_RegisterPage);
             this.Controls.Add(this.label_MemberName_RegisterPage);
             this.Controls.Add(this.panel1);
             this.Name = "MemberLoanCount";
             this.Text = "MemberLoanCount";
+            this.Load += new System.EventHandler(this.MemberLoanCount_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_ReturnDate)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -173,14 +199,16 @@
         #endregion
 
         private System.Windows.Forms.LinkLabel linkLabel1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_Loan;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.TextBox textBox_surnamemember;
-        private System.Windows.Forms.TextBox textBox_namemember;
         private System.Windows.Forms.Label label_memberSurname_RegisterPage;
         private System.Windows.Forms.Label label_MemberName_RegisterPage;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label_MemberUpdate_SignIn;
+        private System.Windows.Forms.Label label_MemberLoanCount_SignIn;
         private System.Windows.Forms.Label label_TitleLogin;
+        private System.Windows.Forms.NumericUpDown nud_ReturnDate;
+        private System.Windows.Forms.DateTimePicker dtp_checkoutDate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_expectedReturnDate;
     }
 }
--- Library/View/Presentation/MemberLoanCount.cs
+++ Library/View/Presentation/MemberLoanCount.cs
@@ -16,5 +16,36 @@ namespace Library.View.Presentation
         {
             InitializeComponent();
         }
+
+        private void MemberLoanCount_Load(object sender, EventArgs e)
+        {
+            nud_ReturnDate.Minimum = 1;
+            nud_ReturnDate.Maximum = 14; //maksimum 14 gün sonra iade edebilir.
+        }
+
+        private void btn_Loan_Click(object sender, EventArgs e)
+        {
+            DateTime checkoutDate = dtp_checkoutDate.Value; //Hangi tarihte kiralamaya başlayacağını aldık kitabı.
+            int loanDays = (int)nud_ReturnDate.Value; //Kaç gün kiralayacağını aldık.
+
+            DateTime expectedReturnDate = checkoutDate.AddDays(loanDays); //Beklenen iade tarihini hesapladık.
+            lbl_expectedReturnDate.Text = "Return By: " + expectedReturnDate.ToShortDateString(); //Beklenen iade tarihini gösterdik, sadece gün ay ve yıl şeklinde.
+            //şimdi kontroller
+            if (checkoutDate > DateTime.Now)
+            {
+                MessageBox.Show("Loan hasn't started yet!", "İnfo!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (DateTime.Now > expectedReturnDate)
+            {
+                MessageBox.Show("Late, book must have been returned before that date!", "İnfo!!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                TimeSpan remains = expectedReturnDate - DateTime.Now; //Kalan süreyi hesapladık.
+                lbl_expectedReturnDate.Text = $"Loan Date: {checkoutDate:dd/MM/yyyy}\n" +
+                $"Loan Duration: {loanDays} days\n" +
+                $"Expected Return: {expectedReturnDate:dd/MM/yyyy}";
+            }
+        }
     }
 }
